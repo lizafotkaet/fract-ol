3b86eb274bf063f3387900c0aab39f2891b9c634
ebarbash <ebarbash@student.codam.nl>
2025-06-14 19:28:30 +0200

    getting started

---
+++ src/fract-ol.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fract-ol.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sergei_pilman <sergei_pilman@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/14 19:08:06 by sergei_pilm       #+#    #+#             */
+/*   Updated: 2025/06/14 19:14:52 by sergei_pilm      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/libft.h"
+#include <mlx.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <linux/types.h>
